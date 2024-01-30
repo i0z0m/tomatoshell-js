56ea2e5068c6142c8baeff508699227e774d0565
i0z0m <i0z0mu@gmail.com>
2024-01-30 16:46:22 +0900

    Separate the class into separate another file

--- tomatoShell.js
+++ tomatoShell.js
@@ -1,44 +1,5 @@
-import { EventEmitter } from 'events';
 import readline from 'readline';
-
-class PomodoroTimer extends EventEmitter {
-  constructor(workDuration, breakDuration) {
-    super();
-    this.workDuration = workDuration;
-    this.breakDuration = breakDuration;
-    this.timerId = null;
-    this.timeLeft = workDuration;
-    this.isWork = true;
-    this.session = 1; // Initialize the session to 1
-  }
-
-  start() {
-    this.timerId = setInterval(() => {
-      this.timeLeft -= 1;
-      if (this.timeLeft >= 0) {
-        this.emit('tick', this.formatTime(this.timeLeft));
-      }
-
-      if (this.timeLeft === 0) {
-        this.stop();
-        this.isWork = !this.isWork;
-        this.timeLeft = this.isWork ? this.workDuration : this.breakDuration;
-        this.session++; // Increment the session
-        this.emit('completed', this.isWork);
-      }
-    }, 1000);
-  }
-
-  stop() {
-    clearInterval(this.timerId);
-  }
-
-  formatTime(seconds) {
-    const minutes = Math.floor(seconds / 60);
-    const remainingSeconds = seconds % 60;
-    return `${minutes.toString().padStart(2, '0')}:${remainingSeconds.toString().padStart(2, '0')}`;
-  }
-}
+import PomodoroTimer from './PomodoroTimer.js';
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -123,3 +84,5 @@ const main = async () => {
 };
 
 main();
+
+export default main;
